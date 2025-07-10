5a974e04e7773153c7aeaaa633985c7cbfc4c9ab
Yucatta <ahmetabali41@gmail.com>
2025-07-10 13:55:46 +0300

    cool stuff

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,8 +2,27 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex flex-row h-15 border-b-4 border-[rgb(100,20,150)] items-center justify-center">
-      <div className="text-3xl font-bold">Header</div>
+    <div className="flex flex-row h-20 border-b-4 border-[rgb(100,20,150)] items-center justify-between">
+      <div className="text-2xl flex flex-row justify-evenly text-[rgb(207,88,253)] w-1/3 font-bold">
+        <a
+          className="  hover:text-[rgb(150,120,200)] cursor-pointer"
+          href="http://localhost:3000/"
+        >
+          Home
+        </a>
+        <a
+          className=" hover:text-[rgb(150,120,200)] cursor-pointer"
+          href="http://localhost:3000/games"
+        >
+          Games
+        </a>{" "}
+        {/* <a href="https://github.com/Yucatta" className="w-10 h-10"></a> */}
+      </div>
+      <div>
+        <a href="https://github.com/Yucatta">
+          <img src={"/logos/github.svg"} className="w-10 h-10 mr-10"></img>
+        </a>
+      </div>
     </div>
   );
 };
