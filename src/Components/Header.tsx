009964b5efeae6cb5984ab40d7c81b301dca37d2
Yucatta <ahmetabali41@gmail.com>
2025-07-10 19:48:32 +0300

    holy local host

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <div className="flex flex-row h-20 border-b-4 border-[rgb(100,20,150)] items-center justify-between">
       <div className="text-2xl flex flex-row justify-evenly text-[rgb(207,88,253)] w-1/3 font-bold">
-        <a
+        <Link
+          href="/"
           className="  hover:text-[rgb(150,120,200)] cursor-pointer"
-          href="http://localhost:3000/"
         >
           Home
-        </a>
-        <a
+        </Link>
+        <Link
           className=" hover:text-[rgb(150,120,200)] cursor-pointer"
-          href="http://localhost:3000/games"
+          href="/games"
         >
           Games
-        </a>{" "}
+        </Link>{" "}
         {/* <a href="https://github.com/Yucatta" className="w-10 h-10"></a> */}
       </div>
       <div>
